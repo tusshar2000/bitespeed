e1522397c3e532c2f9f3993a2180caae5c31cd31
Tusshar Laddha <tussharladdha@Tusshars-MacBook-Pro.local>
2023-07-02 20:35:17 +0530

    contact migration

---
+++ db/migrate/20230702150042_create_contact.rb
@@ -0,0 +1,13 @@
+class CreateContact < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :phone_number
+      t.string :email
+      t.integer :linked_id
+      t.string :link_precedence, null: false
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+  end
+end
